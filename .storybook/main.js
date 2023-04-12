5692cfcd5d111bc9538de2ef3eae5194d9b7f8a7
Jake Ramphal <jacques.r@myplanet.com>
2023-04-12 11:43:44 -0400

    storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
+    "@storybook/addon-a11y",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
