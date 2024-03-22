0a6112278a31efbca3f61f029937008441bee9eb
Jake Ramphal <jacques.ramphal@orium.com>
2024-03-22 10:00:14 -0400

    new board

--- src/_codux/boards/new-board.board.tsx
+++ src/_codux/boards/new-board.board.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { createBoard } from '@wixc3/react-board';
 import { Button } from '../../stories/Button.jsx';
 
+
 export default createBoard({
     name: 'New Board',
     Board: () => <div>
-        <Button size="large" label="Label" backgroundColor="blue" primary={undefined} /></div>,
+<div>
+        <Button size="large" label="Label" backgroundColor="none" primary={undefined} /></div>,
+        </div>,
     isSnippet: true,
     environmentProps: {
         windowWidth: 300
